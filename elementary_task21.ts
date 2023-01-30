dec9b61bd6ddfccaf772188ba7e9557d62fc2263
EnTheos <theosenigma@gmail.com>
2023-01-30 16:16:35 +0300

    elementary task 21

---
+++ elementary_task21.ts
@@ -0,0 +1,20 @@
+const funds: {amount: number}[] = [
+    {amount: -1400},
+    {amount: 2400},
+    {amount: -1000},
+    {amount: 500},
+    {amount: 10400},
+    {amount: -11400}
+];
+
+const getPositiveIncomeAmount = (data: {amount: number}[]): number => {
+    return data.reduce((acc, item) => {
+        return acc + (item.amount > 0 ? item.amount : 0);
+    }, 0);
+};
+
+const getTotalIncomeAmount = (data: {amount: number}[]): number => {
+    return data.some(item => item.amount < 0) ? data.reduce((acc, item) => {
+        return acc + item.amount;
+    }, 0) : getPositiveIncomeAmount(data);
+};
